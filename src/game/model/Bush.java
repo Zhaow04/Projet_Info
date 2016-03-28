27065296e378d392711cf8ed9dc3721dce2d7286
ZhaoWen <zhaowen04@hotmail.com>
2016-03-28 17:05:04 +0200

    Comments

--- src/game/model/Bush.java
+++ src/game/model/Bush.java
@@ -1,33 +1,41 @@
 package game.model;
 
+/**
+ * Extends from {@code Obstacle} <br/>
+ * Basic obstacle.
+ * 
+ * @author ZhaoWen
+ * @see {@link Obstacle}
+ *
+ */
 public class Bush extends Obstacle{
-	
+
 	//****************************** Attributes ******************************
-	
-	
-		//****************************** Constructor ******************************
-		
-		/**
-		 * Creates a bush and sets the map on which it is and its position.
-		 * 
-		 * @param map
-		 * @param x
-		 * @param y
-		 * @see {@link Obstacle#Obstacle(Map)}
-		 */
-		public Bush (Map map, int x, int y){
-			super(map);
-			setPosition(x,y);
-			
-			setImageURL("game/model/treecomp.PNG");
-			setOffsetX(290);
-			setOffsetY(130);
-			setWidth(90);
-			setHeight(90);
-		}
-		
-		//************************** Getters and Setters **************************
-		
-		//******************************** Methods ********************************
-	
+
+
+	//****************************** Constructor ******************************
+
+	/**
+	 * Creates a bush and sets the map on which it is and its position.
+	 * 
+	 * @param map
+	 * @param x
+	 * @param y
+	 * @see {@link Obstacle#Obstacle(Map)}
+	 */
+	public Bush (Map map, int x, int y){
+		super(map);
+		setPosition(x,y);
+
+		setImageURL("game/model/treecomp.PNG");
+		setOffsetX(290);
+		setOffsetY(130);
+		setWidth(90);
+		setHeight(90);
+	}
+
+	//************************** Getters and Setters **************************
+
+	//******************************** Methods ********************************
+
 }
--- src/game/model/Donjon.java
+++ src/game/model/Donjon.java
@@ -1,86 +1,94 @@
 package game.model;
 
+/**
+ * Extends from {@code ViewableObject} <br/>
+ * Special environment with high level monsters and a boss.
+ * 
+ * @author ZhaoWen
+ * @see {@link ViewableObject}
+ *
+ */
 public class Donjon extends ViewableObject {
-	
+
 	//****************************** Attributes ******************************
-	
-		private Map currentMap;
-		private int[] position; // w/o initialization
-		
-		//****************************** Constructor ******************************
-		
-			/**
-			 * Creates a donjon on the current map.
-			 * 
-			 * @param map
-			 * @see {@link ViewableObject}
-			 */
-			public Donjon (Map map, int x, int y){
-				setCurrentMap(map);
-				setPosition(x,y);
-				
-				setImageURL("game/model/safehouse.png");
-				setOffsetX(250);
-				setOffsetY(180);
-				setWidth(170);
-				setHeight(170);
-			}
-			
-		//************************** Getters and Setters **************************
-			
-			/**
-			 * Gets the map on which the donjon currently is.
-			 * 
-			 * @return current map
-			 */
-			protected Map getCurrentMap(){
-				return currentMap;
-			}
-			
-			/**
-			 * Sets the map on which the donjon currently is.
-			 * 
-			 * @param map
-			 */
-			private void setCurrentMap(Map map){
-				currentMap = map;
-			}
-			public int[] getPosition(){
-				return position;
-			}
-			
-			/**
-			 * Sets the position to (x,y).
-			 * 
-			 * @param x
-			 * @param y
-			 */
-			protected void setPosition(int x, int y){
-				position = new int[2];
-				position[0] = x; position[1] = y;
-				currentMap.setOccupied(x, y);
-			}
-			//******************************** Methods ********************************
-			
-			/**
-			 * Sets the position to (x,y). Used when a previous position is defined.
-			 * 
-			 * @param x
-			 * @param y
-			 */
-			protected void setNewPosition(int x,int y){
-				setPosition(x,y);
-				currentMap.addDonjonOnMap(this);
-			}
-			
-			/**
-			 * Sets the position (x,y) to empty (no donjon).
-			 * 
-			 * @param x
-			 * @param y
-			 */
-			public void emptyPosition(int x, int y){
-				getCurrentMap().removeDonjonOnMap(x, y);
-				getCurrentMap().setEmpty(x,y);
-			}
+
+	private Map currentMap;
+	private int[] position; // w/o initialization
+
+	//****************************** Constructor ******************************
+
+	/**
+	 * Creates a donjon on the current map.
+	 * 
+	 * @param map
+	 * @see {@link ViewableObject}
+	 */
+	public Donjon (Map map, int x, int y){
+		setCurrentMap(map);
+		setPosition(x,y);
+
+		setImageURL("game/model/safehouse.png");
+		setOffsetX(250);
+		setOffsetY(180);
+		setWidth(170);
+		setHeight(170);
+	}
+
+	//************************** Getters and Setters **************************
+
+	/**
+	 * Gets the map on which the donjon currently is.
+	 * 
+	 * @return current map
+	 */
+	protected Map getCurrentMap(){
+		return currentMap;
+	}
+
+	/**
+	 * Sets the map on which the donjon currently is.
+	 * 
+	 * @param map
+	 */
+	private void setCurrentMap(Map map){
+		currentMap = map;
+	}
+	public int[] getPosition(){
+		return position;
+	}
+
+	/**
+	 * Sets the position to (x,y).
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	protected void setPosition(int x, int y){
+		position = new int[2];
+		position[0] = x; position[1] = y;
+		currentMap.setOccupied(x, y);
+	}
+	//******************************** Methods ********************************
+
+	/**
+	 * Sets the position to (x,y). Used when a previous position is defined.
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	protected void setNewPosition(int x,int y){
+		setPosition(x,y);
+		currentMap.addDonjonOnMap(this);
+	}
+
+	/**
+	 * Sets the position (x,y) to empty (no donjon).
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void emptyPosition(int x, int y){
+		getCurrentMap().removeDonjonOnMap(x, y);
+		getCurrentMap().setEmpty(x,y);
+	}
 }
